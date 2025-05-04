4d23b4d1fbd84234c53d4fe5dceec7fd77a2317b
ciroica621 <mondaine.rn@gmail.com>
2025-05-04 16:59:48 +0900

    chatgpt.py pushed

--- backend/chatgpt.py
+++ backend/chatgpt.py
@@ -1,9 +1,11 @@
 import openai
 import os
+import feedback
 from dotenv import load_dotenv
 
 load_dotenv()
 
+print(os.getenv("API_KEY"))
 openai.api_key = os.getenv("API_KEY")
 
 usr_schedule = str(input("キャンセルしたい予定を入力してください："))
@@ -45,3 +47,41 @@ res_2 = openai.chat.completions.create(
 )
 
 print(res_2.choices[0].message.content)
+
+while True:
+    usr_aite = str(input("言い訳を行う相手を入力してください。:"))
+    while True:
+        usr_taido = tuple(
+            input(
+                "どのような態度の言い訳の文章がより望ましいか、以下のオプションの中から当てはまるだけ複数選択してください。\n1.丁寧\n2.カジュアル\n3.申し訳なさ強め\n4.ややおびえる感じ\n5.すごく反省している感じ\n6.まったく反省していない感じ\n7.傲慢で見下す感じ\n:"
+            ).split()
+        )
+        for int_str in usr_taido:
+            if int_str.isdigit() == False:
+                print("整数で入力してください。")
+                break
+        else:
+            int_usr_taido = tuple([int(i) for i in usr_taido])
+            print(int_usr_taido)
+            """
+            if len(int_usr_taido) == 1:
+                int_usr_taido = int_usr_taido[0]
+            """
+            break
+    res = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "日本語で返答してください。",
+            },
+            {
+                "role": "system",
+                "content": feedback.feedback(usr_aite, *int_usr_taido),
+            },
+        ],
+    )
+    print(res.choices[0].message.content)
+    yes_or_no = input("\n返信はこれでよろしいですか？ はい/いいえ:")
+    if yes_or_no == "はい":
+        break
