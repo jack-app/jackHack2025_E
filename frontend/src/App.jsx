af9714fc36e2fbcdeb79aa5168e38cf330bb0381
Barnum8639 <sutanai1925@gmail.com>
2025-05-04 16:24:37 +0900

    入力ボックス

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { React, useState } from 'react'
 import './App.css'
+import { Button,TextField } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,21 @@ function App() {
 <div class="header">キャンセル代行CanCan</div>
 <div class="body">
     <div class="main">
-    <p>a------</p>
+    <TextField
+      id="standard-basic" label="キャンセルしたい予定を入力" variant="standard" multiline maxRows={4}
+      sx={{position:'absolute',
+        width:'500px',
+        left: '10%',
+        bottom : '5%',
+        backgroundColor: '#f9f9f9',}}>
+    </TextField>
+    <Button variant="contained"
+      sx={{ position: 'absolute',
+            bottom: '5%',
+            left:'50%',
+      }}>
+      送信
+    </Button>
     </div>
     <div class="setting">
       <button class="GObutton">GO</button>
