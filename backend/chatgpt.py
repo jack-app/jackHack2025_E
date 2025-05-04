5c4c44d8452f7d34f423e5d1c9cf36353f970561
ciroica621 <mondaine.rn@gmail.com>
2025-05-04 17:40:30 +0900

    change chatgpt.py

--- backend/chatgpt.py
+++ backend/chatgpt.py
@@ -50,7 +50,7 @@ print(res_2.choices[0].message.content)
 
 while True:
     usr_aite = str(input("言い訳を行う相手を入力してください。:"))
-    while True:
+    while True:  # 整数で入力されていない場合は受け付けない
         usr_taido = tuple(
             input(
                 "どのような態度の言い訳の文章がより望ましいか、以下のオプションの中から当てはまるだけ複数選択してください。\n1.丁寧\n2.カジュアル\n3.申し訳なさ強め\n4.ややおびえる感じ\n5.すごく反省している感じ\n6.まったく反省していない感じ\n7.傲慢で見下す感じ\n:"
@@ -62,11 +62,6 @@ while True:
                 break
         else:
             int_usr_taido = tuple([int(i) for i in usr_taido])
-            print(int_usr_taido)
-            """
-            if len(int_usr_taido) == 1:
-                int_usr_taido = int_usr_taido[0]
-            """
             break
     res = openai.chat.completions.create(
         model="gpt-4o-mini",
