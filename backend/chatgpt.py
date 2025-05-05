e06939c4aa306d3aedf731732a5d702c7528f6e1
ciroica621 <mondaine.rn@gmail.com>
2025-05-05 13:36:59 +0900

    例外処理部分のリファクタリング

--- backend/chatgpt.py
+++ backend/chatgpt.py
@@ -28,9 +28,9 @@ reasons_res = openai.chat.completions.create(
     ],
 )
 
-while True:
+while True:  # 整数で入力されていない場合は受け付けない
     try:
-        usr_aite = int(
+        aite_num = int(
             input(
                 "言い訳を行う相手を入力してください。以下のオプションの中から当てはまるだけ複数選択してください。:\n1.上司\n2.友達\n3.恋人\n4.同僚\n5.先輩\n:"
             )
@@ -90,13 +90,15 @@ res_1 = openai.chat.completions.create(model="gpt-4o-mini", messages=message)
 
 
 while True:  # 整数で入力されていない場合は受け付けない
-    usr_taido = tuple(
-        input(
-            "どのような態度の言い訳の文章がより望ましいか、以下のオプションの中から当てはまるだけ複数選択してください。\n1.丁寧\n2.カジュアル\n3.申し訳なさ強め\n4.ややおびえる感じ\n5.すごく反省している感じ\n6.まったく反省していない感じ\n7.傲慢で見下す感じ\n:"
-        ).split()
-    )
     try:
-        int_usr_taido = tuple(int(i) for i in usr_taido)
+        taido_nums = tuple(
+            int(i)
+            for i in tuple(
+                input(
+                    "どのような態度の言い訳の文章がより望ましいか、以下のオプションの中から当てはまるだけ複数選択してください。\n1.丁寧\n2.カジュアル\n3.申し訳なさ強め\n4.ややおびえる感じ\n5.すごく反省している感じ\n6.まったく反省していない感じ\n7.傲慢で見下す感じ\n:"
+                ).split()
+            )
+        )
         break
     except ValueError:
         print("整数で入力してください。")
@@ -109,7 +111,7 @@ res_2 = openai.chat.completions.create(
         },
         {
             "role": "system",
-            "content": feedback.feedback(usr_aite, *int_usr_taido),
+            "content": feedback.feedback(aite_num, *taido_nums),
         },
     ],
 )
