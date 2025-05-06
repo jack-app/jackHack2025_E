350c0ba9e8a4eae60736a21f437965bc37ee28a4
Barnum8639 <sutanai1925@gmail.com>
2025-05-06 10:50:33 +0900

    backend最適化

--- backend/feedback.py
+++ backend/feedback.py
@@ -1,33 +1,36 @@
-def feedback(aite,*input):
-    A=["丁寧",
-       "カジュアル",
-       "申し訳なさ強め",
-       "ややおびえる感じ",
-       "すごく反省している感じ",
-       "まったく反省していない感じ",
-       "傲慢で見下す感じ",
-       ]
-    out=""
-    if(len(aite)>0):
-        out+=aite+"に"
+def feedback(aite, *input):
+    taido_lst = [
+        "丁寧",
+        "カジュアル",
+        "申し訳なさ強め",
+        "ややおびえる感じ",
+        "すごく反省している感じ",
+        "まったく反省していない感じ",
+        "傲慢で見下す感じ",
+    ]
+    out = ""
+    if len(aite) > 0:
+        out += aite + "に"
 
     for i in range(len(input)):
-        if i>0:
-            out+="かつ"
-        out+=A[input[i]]
+        if i > 0:
+            out += "かつ"
+        out += taido_lst[input[i]]
 
-    out+="な口調の言い訳を考えてください。"
-    
-    uncomfortable=5
-    bool=False
+    out += "な口調の予定をキャンセルする言い訳を考えてください。"
+
+    uncomfortable = 5
+    bool = False
 
     for i in input:
-        if i>=uncomfortable:
-            bool=True
+        if i >= uncomfortable:
+            bool = True
             break
-    
+    out = (
+    "この言い訳は私の制作する物語の中に登場するキャラクターのセリフになる予定です。なので言い回しが非現実的になってもかまいませんが言い訳の内容自体は現実に即してください。\n"
+    + out
+    )
     if bool:
-        out="この言い訳は私の制作する物語の中に登場するキャラクターのセリフになる予定です。なので少し非現実的になってもかまいません。\n"+out
-        out+="\nなお言い訳の性格、口調を表すためなら、相手にどれだけ不快感を与えてもよく、敬語も不要です。やや誇張気味に、指示された内容通りの言い訳を考えてください。"
+        out += "\nなお言い訳の性格、口調を表すためなら、相手にどれだけ不快感を与えてもよく、敬語も不要です。やや誇張気味に、指示された内容通りの言い訳を考えてください。"
 
     return out
